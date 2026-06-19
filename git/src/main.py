80e5267b36609eabe71750e8db3b55db3f4e503f
s1pp1nw4ter <iliagod1337@gmail.com>
2026-06-19 21:55:47 +0300

    feat: changes

--- git/src/main.py
+++ git/src/main.py
@@ -1,36 +1,19 @@
-class Order:
-    TAX_RATE = 0.08  # 8% налог
-    SERVICE_CHARGE = 0.05  # 5% сервисный сбор
-
-    def __init__(self, customer):
-        self.customer = customer
-        self.dishes = []
-
-    def add_dish(self, dish):
-        if isinstance(dish, Dish):
-            self.dishes.append(dish)
-        else:
-            raise ValueError("Можно добавлять только объекты класса Dish.")
-
-    def calculate_total(self):
-        return sum(dish.price for dish in self.dishes)
-
-    def __str__(self):
-        dish_list = "\n".join([str(dish) for dish in self.dishes])
-        return f"Order for {self.customer.name}:\n{dish_list}\nTotal: ${self.calculate_total():.2f}"
+import json
+import os
 
 
-class Dish:
-    def __init__(self, name, price, category):
-        self.name = name
-        self.price = price
-        self.category = category
+def load_books(filename='library.json'):
+    """Загрузка списка книг из JSON-файла."""
+    if not os.path.isfile(filename):
+        return []
+    with open(filename, 'r', encoding='utf-8') as file:
+        try:
+            return json.load(file)
+        except json.JSONDecodeError:
+            return []
 
-    def __str__(self):
-        return f"Dish: {self.name}, Category: {self.category}, Price: ${self.price:.2f}"
 
-
-class Customer:
-    def __init__(self, name, membership="Regular"):
-        self.name = name
-        self.membership = membership
+def saving_books(books, filename='library.json'):
+    """Сохранение списка книг в JSON-файл."""
+    with open(filename, 'w', encoding='utf-8') as file:
+        json.dump(books, file, ensure_ascii=False, indent=4)
